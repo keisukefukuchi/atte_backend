b977f9c1bdf4e537db78f33856faa5fad04230ec
keisukefukuchi <kekke53530708@gmail.com>
2022-07-08 16:16:49 +0900

    Add: relation

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -10,4 +10,14 @@ class Attendance extends Model
     use HasFactory;
 
     protected $fillable = ['user_id','date','start_time','end_time'];
+
+    public function users()
+    {
+        return $this->belongsTo('App\Models\User', "user_id");
+    }
+    
+    public function rests()
+    {
+        return $this->hasMany('App\Models\Rest');
+    }
 }
